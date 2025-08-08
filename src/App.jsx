deb710d8e83d945d63ac86cea7b9fdb9382ed9c5
Zhoha Damani <zhohadamani@Zhohas-MacBook-Air.local>
2025-08-07 23:59:39 -0400

    added API code to pull from The movies API

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,81 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Search from "./components/Search.jsx";
+import Spacer from "./components/Spacer.jsx";
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_TOKEN = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_TOKEN}`,
+    }
+}
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [movieList, setMovieList] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+
+    const fetchMovies = async () => {
+        setIsLoading(true);
+        setErrorMessage("")
+
+        try {
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            const response = await fetch(endpoint, API_OPTIONS);
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch movies, try again later!!')
+            }
+            const data = await response.json();
+
+            if (data.Response === 'false') {
+                setErrorMessage(data.Error || "Failed to fetch movies");
+                setMovieList([]);
+                return;
+            }
+            setMovieList(data.results || []);
+
+        } catch (error) {
+            console.error(`Error fetching movies ${error}`)
+            setErrorMessage('Error fetching Movies, please try again later :)')
+        } finally {
+            setIsLoading(false)
+        }
+
+    }
+    useEffect(() => {
+        fetchMovies()
+    }, []);
     return (
         <main>
-            <div className="pattern" />
-            <div className="wrapper" >
+            <div className="pattern"/>
+            <div className="wrapper">
                 <header>
-                    <img src='../hero-img.png' alt="Hero Banner" />
+                    <img src='../hero-img.png' alt="Hero Banner"/>
                     <h1 className="title">Movies <span className="text-gradient"> Tailored </span>For You!!</h1>
-                    <p className="text-white text-center">
-                       <Search searchTerm = {searchTerm} setSearchTerm={setSearchTerm} />
-                    </p>
-
+                    <div className="text-white text-center">
+                        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+                    </div>
                 </header>
+                <Spacer/>
+                <section className='all-movies'>
+                    <h2>All Movies</h2>
+                    {isLoading ? (
+                        <p className="text-white">Loading....</p>
+                    ) : errorMessage ? (
+                        <p className="text-white">{errorMessage}</p>
+                    ) : (
+                        <ul className="text-white space-y-2">
+                            {movieList.map((movie) => (
+                                <li key={movie.id}>{movie.title}</li>
+                            ))}
+                        </ul>
+                    )}
+                </section>
             </div>
         </main>
     );
---
+++ src/components/Spacer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Spacer = () => {
+    return (
+        <div className="h-4 sm:h-6 md:h-8 lg:h-12 xl:h-16" />
+    );
+};
+
+export default Spacer;
